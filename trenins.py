4b853f32ac31f8ff4386b4e6de0bb93ccdeb80f6
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-03-26 14:21:02 +0200

    Update trenins.py

--- trenins.py
+++ trenins.py
@@ -1,3 +1,33 @@
-vards = "Sveiki!"
+def rezultats(sk1, sk2):
+    if sk1<6 and sk2<6:
+        rez = sk1*sk2
+    else:
+        rez = sk1+sk2
+        return rez
 
-print(vards)
+
+for skaitlis in range(1,11, 2): 
+    for otrs in range(2,11):
+        print("mūsu skaitlis:", skaitlis, "rezultāts:", rezultats(4, skaitlis))
+
+
+def zvaigznites(skaits):
+    for rindasNr in range(1, skaits+1):
+        for zvaigzne in range(rindasNr)
+
+        print("*")
+
+
+skaitlis1 = 7
+skaitlis2 = 3
+
+print("pirmais skaitlis:", skaitlis1, "otrais skaitlis:", skaitlis2, "summa:", summa(skaitlis1,skaitlis2))
+
+pirmais = "6"
+
+print(pirmais)
+
+vards2 = "Nē"
+
+print(pirmais, vards2)
+
