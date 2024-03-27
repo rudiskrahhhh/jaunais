0c64211c2f5638a6feb5a427123e11ff93fc9b7a
rudiskrahhhh <rudis.kruskops@gmail.com>
2024-03-27 12:40:35 +0200

    Update trenins.py

--- trenins.py
+++ trenins.py
@@ -3,31 +3,23 @@ def rezultats(sk1, sk2):
         rez = sk1*sk2
     else:
         rez = sk1+sk2
-        return rez
+
+    return rez
+
+for skaitlis in range(1, 11, 2):        #range - funkcija, kas skaita skaitļus
+    for otrs in range(2, 11, 2):
+        print("pirmais skaitlis:", skaitlis,"otrais skaitlis:", otrs, "rezultāts: ", rezultats(skaitlis, otrs))
 
 
-for skaitlis in range(1,11, 2): 
-    for otrs in range(2,11):
-        print("mūsu skaitlis:", skaitlis, "rezultāts:", rezultats(4, skaitlis))
-
-
-def zvaigznites(skaits):
+def zvaigznites1(skaits):
     for rindasNr in range(1, skaits+1):
         for zvaigzne in range(rindasNr):
+            print("*", end="")
+        print("")
 
-        print("*")
+def zvaigznites2(skaits):
+    for rindasNr in range(1, skaits+1):
+        print("*"*rindasNr)
 
 
-skaitlis1 = 7
-skaitlis2 = 3
-
-print("pirmais skaitlis:", skaitlis1, "otrais skaitlis:", skaitlis2, "summa:", summa(skaitlis1,skaitlis2))
-
-pirmais = "6"
-
-print(pirmais)
-
-vards2 = "Nē"
-
-print(pirmais, vards2)
-
+zvaigznites1(7)
